2575b2d9020e2fc741504ec4b96e18a3117d0b2a
Joeycheng02 <Joey.Cheng@student.uantwerpen.be>
2018-08-25 15:02:03 +0200

    SimulationTest.cpp final version

--- SimulationTest.cpp
+++ SimulationTest.cpp
@@ -23,7 +23,13 @@ TEST_F(SimulationTest, DefCon) {
     EXPECT_EQ(0, int(simulation.getAirplanes().size()));
     EXPECT_EQ(0, int(simulation.getRunways().size()));
     EXPECT_TRUE(runway.isVacant());
-
+    EXPECT_TRUE(simulation.properlyInitialized());
+    Simulation* a = &simulation;
+    EXPECT_TRUE(a->properlyInitialized());
+    Simulation b = simulation;
+    EXPECT_FALSE(b.properlyInitialized());
+    simulation.getRunways().push_back(&runway);
+    EXPECT_EQ(1, int(simulation.getRunways().size()));
 
 
     remove("console_output.txt");
